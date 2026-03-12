993655753134cf09873b4a780d7ebf49bc5e979d
Vishalkrishna3434 <vishalthonukunooru@gmail.com>
2026-03-13 00:40:09 +0530

    created basic page with global context

---
+++ src/context/GlobalState.js
@@ -0,0 +1,23 @@
+import React, { Children, createContext, useReducer } from "react";
+import AppReducer from './AppReducer';
+
+const InitialState = {
+  transactions: [
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 }
+  ]
+}
+
+//create Context
+export const GlobalContext = createContext(InitialState);
+
+//provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, InitialState);
+  return (<GlobalContext.Provider value={{ transactions: state.transactions }}>
+    {children}
+  </GlobalContext.Provider>
+  );
+}
